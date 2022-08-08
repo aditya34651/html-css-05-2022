bde0d6a7004ae940c09de7c703d377ab3d9d7963
aditya34651 <aditya34651@gmail.com>
2022-08-08 11:17:42 +0530

    New files and changes

---
+++ 3.Html Dom/app.js
@@ -0,0 +1,7 @@
+console.log(document.getElementById("header").innerText="<h1>Hello</h1>")
+console.log(document.getElementById("header").innerHTML="<h1>Hello</h1>")
+document.getElementById("header").style.backgroundColor = "turquoise"
+
+function clickMe(){
+    alert("did you click me?")
+}
